9880724ba4b1002683ea2d680c5f4ab7b21d95c6
Thadchanon <thadchanonjirarat@gmail.com>
2023-10-03 14:49:00 +0700

    useState demo

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,21 +1,26 @@
 import { PostDTO } from './types/dto'
 import classes from './Post.module.css'
+import { useState } from 'react'
 
 interface IPostProps {
   post: PostDTO
 }
 
 const Post = ({ post }: IPostProps) => {
-  const handleClick = (id: number) => {
-    alert(`clicked ${id}`)
+  const [showButton, setShowButton] = useState<boolean>(false)
+
+  const handleClick = () => {
+    setShowButton(!showButton)
   }
 
   return (
-    <div onClick={() => handleClick(post.id)} className={classes.post}>
+    <div className={classes.post}>
       <p>id: {post.id}</p>
       <p>postedBy: {post.userId}</p>
       <p>title: {post.title}</p>
       <p>body: {post.body}</p>
+      {showButton && <p>more post information...</p>}
+      <button onClick={handleClick}>{showButton ? 'Show less' : 'Show more'}</button>
     </div>
   )
 }
