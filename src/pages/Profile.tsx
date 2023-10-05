12efd4d454647bf4c2fc9c8fc4133d1a01de871e
Thadchanon <thadchanonjirarat@gmail.com>
2023-10-05 16:24:04 +0700

    react router

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,11 @@
+import Greeting from '../components/Greeting'
+
+const Profile = () => {
+  return (
+    <div>
+      <Greeting name="Job" isLoggedIn={true} />
+    </div>
+  )
+}
+
+export default Profile
